e6172302bec51728a69ec15f423d263eb3ec4159
wslwd <wslwdljh@163.com>
2021-03-07 17:11:23 +0800

    分支添加dev

--- admin-login/src/main/java/com/ljh/admin/LoginMain.java
+++ admin-login/src/main/java/com/ljh/admin/LoginMain.java
@@ -11,6 +11,7 @@ public class LoginMain {
     public static void main(String[] args) {
         SpringApplication.run(LoginMain.class,args);
         System.out.println("创建分支dev");
+        System.out.println("分支添加dev");
     }
 
 }
